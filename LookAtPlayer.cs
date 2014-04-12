aef6ff81f4e3b4384c549ace3988b49385c871ff
MohammadKOmer <okm3@njit.edu>
2014-04-12 19:14:41 -0400

    Mohammad Omer Commit

---
+++ LookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+	public class LookAtPlayer : MonoBehaviour
+	{
+	public string url = "http://www.gunnerkrigg.com/comics/00001339.jpg";		public Camera m_Camera;
+	IEnumerator  Start(){
+			WWW www = new WWW(url);
+			yield return www;
+			renderer.material.mainTexture = www.texture;
+		}
+		void Update()
+		{
+			transform.LookAt(transform.position + m_Camera.transform.rotation * -Vector3.back,
+			                 m_Camera.transform.rotation * Vector3.up);
+		}
+	}
