f99b452be8ec3f449e5994d59be4585d5490960c
Nkelliny <sitelegion@coolsite.net>
2014-04-12 20:34:59 -0400

    Update database.cs

--- database.cs
+++ database.cs
@@ -2,8 +2,7 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.Oleb;
-using UnityEngine;
-using System.Collections;
+
 
 class databasepullup
 {
@@ -19,19 +18,4 @@ class databasepullup
 		client.DefaultRequestHeaders.Accept.Clear();
 		client.DEfaultREquestHeaders.Accept.Add("New Database Type");
 	{
-
-public class Timescript : MonoBehaviour
-{
-	public float timer = 0.0f;
-	public bool timerstarted = false;
-
-	void update()
-	{
-		if (input.GetKeyDown (KeyCode.DownArrow)
-			
-			if (timerstarted = true)
-			if timerstarted == false;
-			else timerstarted == true;
-	}
-
-	
+}
