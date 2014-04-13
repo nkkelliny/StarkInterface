f4191021566f0cedc45bc08818e9ecefd7c7dba7
Nkelliny <sitelegion@coolsite.net>
2014-04-13 11:51:28 -0400

    Create python.cs

---
+++ python.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO
+
+class Python Program
+{
+	private void run_cmd(string cmd, string args)
+{
+     ProcessStartInfo start = new ProcessStartInfo();
+     start.FileName = "my/full/path/to/python.exe";
+     start.Arguments = string.Format("{0} {1}", cmd, args);
+     start.UseShellExecute = false;
+     start.RedirectStandardOutput = true;
+     using(Process process = Process.Start(start))
+     {
+         using(StreamReader reader = process.StandardOutput)
+         {
+             string result = reader.ReadToEnd();
+             Console.Write(result);
+         }
+     }
+}
